976ebace191298cc53b862a03b69f0465d170552
Juan Esteban Zapata Roldan <juan.zapata@cadena.com.co>
2026-07-27 11:23:37 -0500

    Configurar despliegue para Render

---
+++ usuarios/management/commands/createadmin.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = "Crear un superusuario automáticamente"
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+
+        username = "admin"
+        email = "admin@bullstrucks.com"
+        password = "Admin123456"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password,
+            )
+            self.stdout.write(
+                self.style.SUCCESS("✅ Superusuario creado correctamente.")
+            )
+        else:
+            self.stdout.write(
+                self.style.WARNING("⚠ El superusuario ya existe.")
+            )
